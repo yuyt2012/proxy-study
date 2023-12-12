66ed874529d8127d166913867e6b1308269d963b
yuyt2012 <yuyt2012@gmail.com>
2023-12-12 14:28:44 +0900

    12/12

---
+++ src/main/java/hello/proxy/config/v1_proxy/concrete_proxy/OrderControllerConcreteProxy.java
@@ -0,0 +1,32 @@
+package hello.proxy.config.v1_proxy.concrete_proxy;
+
+import hello.proxy.app.v2.OrderControllerV2;
+import hello.proxy.app.v2.OrderServiceV2;
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+
+public class OrderControllerConcreteProxy extends OrderControllerV2 {
+
+    private final OrderControllerV2 tartget;
+    private final LogTrace logTrace;
+
+    public OrderControllerConcreteProxy(OrderControllerV2 tartget, LogTrace logTrace) {
+        super(null);
+        this.tartget = tartget;
+        this.logTrace = logTrace;
+    }
+
+    @Override
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderController.request()");
+            String result = tartget.request(itemId);
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
---
+++ src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
@@ -0,0 +1,38 @@
+package hello.proxy.jdkdynamic;
+
+import hello.proxy.jdkdynamic.code.AImpl;
+import hello.proxy.jdkdynamic.code.AInterface;
+import hello.proxy.jdkdynamic.code.BImpl;
+import hello.proxy.jdkdynamic.code.BInterface;
+import hello.proxy.jdkdynamic.code.TimeInvocationHandler;
+import java.lang.reflect.Proxy;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class JdkDynamicProxyTest {
+
+    @Test
+    void dynamicA() {
+        AInterface target = new AImpl();
+        TimeInvocationHandler timeInvocationHandler = new TimeInvocationHandler(target);
+        AInterface proxy = (AInterface) Proxy.newProxyInstance(AInterface.class.getClassLoader(),
+                new Class[]{AInterface.class},
+                timeInvocationHandler);
+        proxy.call();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+    }
+
+    @Test
+    void dynamicB() {
+        BInterface target = new BImpl();
+        TimeInvocationHandler timeInvocationHandler = new TimeInvocationHandler(target);
+        BInterface proxy = (BInterface) Proxy.newProxyInstance(BInterface.class.getClassLoader(),
+                new Class[]{BInterface.class},
+                timeInvocationHandler);
+        proxy.call();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+    }
+}
